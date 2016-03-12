4fd540abaabeff7f5137cc201eee632f6a76d1fd
Gabriel.E - <Gabriel.E ->
2016-03-12 13:31:01 -0430

    CRUD completo para carros sin la piezas por carro

--- autosApp/app/controllers/cars_controller.rb
+++ autosApp/app/controllers/cars_controller.rb
@@ -1,24 +1,48 @@
 class CarsController < ApplicationController
 
+def index
+    @car = Car.all
+  end
+
     def show
       @car = Car.find(params[:id])
     end
 
   def new
+     @car = Car.new
   end
 
+  def edit
+    @car = Car.find(params[:id])
+  end
 
    def create
-
          @car = Car.new(cars_params)
-         @car.save
-         redirect_to @car
-
-
+          if @car.save
+             redirect_to @car
+           else
+             render 'new'
+           end
    end
+
+   def update
+     @car = Car.find(params[:id])
+
+     if @car.update(cars_params)
+       redirect_to @car
+     else
+       render 'edit'
+     end
+   end
+
+   def destroy
+       @car = Car.find(params[:id])
+       @car.destroy
+       redirect_to root_path
+     end
 
    private
      def cars_params
-       params.require(:cars).permit(:marca, :modelo, :lat, :long)
+       params.require(:car).permit(:marca, :modelo, :lat, :long)
      end
 end
--- autosApp/app/models/car.rb
+++ autosApp/app/models/car.rb
@@ -1,8 +1,10 @@
 class Car < ActiveRecord::Base
-    after_initialize :init
+    validates :marca, presence: true
+    validates :modelo, presence: true
 
+    after_initialize :init
        def init
          self.lat  ||= rand(-85.00...85.00)           #will set the default value only if it's nil
-         self.long ||= rand(-180.00...180.00) #let's you set a default association
+         self.long ||= rand(-180.00...180.00)         #ill set the default value only if it's nil
        end
 end
